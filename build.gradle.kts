909f46034fa40ecc02683a258e577d484f6d4ce3
Valentin Lezhnin <v888.rus@gmail.com>
2026-09-13 00:27:30 +0200

    added dependencies to gradle build kts and smoke test in main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,17 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    // JSON
+    implementation("tools.jackson.core:jackson-core:3.0.4")
+    implementation("tools.jackson.core:jackson-databind:3.0.4")
+
+    // Logging
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.32")
+    runtimeOnly("ch.qos.logback:logback-core:1.5.32")
+}
+
 java {
     toolchain {
         // Используем Java 25 во всех окружениях
--- src/main/java/pro/_21it/readlater/ReadLaterApplication.java
+++ src/main/java/pro/_21it/readlater/ReadLaterApplication.java
@@ -1,9 +1,21 @@
 package pro._21it.readlater;
 
+import tools.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ReadLaterApplication {
     public static void main(String[] args) {
         ConsoleBanner.print();
 
-        System.out.println("Readlater Starter is running");
+        Logger logger = LoggerFactory.getLogger(ReadLaterApplication.class);
+        logger.info("Readlater Starter is running");
+
+        String json = new ObjectMapper().writeValueAsString(new Ping("UP"));
+        System.out.println(json);
     }
+
+    private static record Ping(String status) {
+    };
+
 }
