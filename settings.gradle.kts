5af1e7422b33d91f132d8952939ec1cdc36ea1b5
Valentin Lezhnin <v888.rus@gmail.com>
2026-09-15 21:30:19 +0200

    added gradle repository todownload specific java

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
+// If a project asks for a Java version that isn't installed,
+// use the Foojay API to find the download link for it and install it automatically.
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "gradle-test"
